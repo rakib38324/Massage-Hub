18e15878ae7ad1614012b09b0b75d93d19b3af67
rakib38324 <aminul.rakib2022@gmail.com>
2024-08-02 16:10:51 +0600

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className="text-4xl text-center">Hello Bangladesh</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
